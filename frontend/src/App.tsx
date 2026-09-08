1f040f382780dc21d251eae232bba2f136559701
dev-lumi <kishoreks1213@gmail.com>
2026-09-08 09:18:19 +0530

    small change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,20 +5,6 @@ import Home from "./pages/Home"
 
 const App = () => {
   return <Home></Home>
-  
-  // return (
-  //   <>
-  //   <Navbar></Navbar>
-  //   <Routes>
-  //     <Route path="/" element={<Home/>}>
-  //     <Route path="/create" element={<CreateNote/>}/>
-  //     <Route path="/edit/:id" element={<EditNote/>}/>
-  //     </Route>
-  //   </Routes>
-
-    
-  //   </>
-  // )
 }
 
 export default App
