d64aae8b6ec8f18b180ed3d2661633b91a6c9cd8
dev-lumi <kishoreks1213@gmail.com>
2026-09-09 21:04:46 +0530

    tiny change

--- frontend/src/components/NoteCard.tsx
+++ frontend/src/components/NoteCard.tsx
@@ -10,7 +10,7 @@ const NoteCard = ({ note, onDelete, onEdit }: NoteCardProps) => {
   return (
     <div className="px-2 py-3 rounded h-50 shadow-xl ring ">
       <h2 className="text-xl font-bold">{note.title} </h2>
-      <p className="text-[14px] font-semibold text-gray-700">{note.content} </p>
+      <p className="text-[16px] font-semibold text-gray-700">{note.content} </p>
       <p className="capitalise font-bold">{note.category}</p>
       <div className="mt-2">
         <button
